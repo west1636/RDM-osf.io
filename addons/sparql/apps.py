a4aa42a45321c35e29d445aad2cb752266de6396
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2018-11-05 11:32:30 +0900

    SPARQLアドオン追加

---
+++ addons/sparql/apps.py
@@ -0,0 +1,30 @@
+from addons.base.apps import BaseAddonAppConfig
+
+
+class SparqlAddonAppConfig(BaseAddonAppConfig):
+
+    name = 'addons.sparql'
+    label = 'addons_sparql'
+    full_name = 'SPARQL'
+    short_name = 'sparql'
+    owners = ['node']
+    added_default = ['node']
+    added_mandatory = []
+    views = ['widget']
+    configs = []
+    categories = ['other']
+    include_js = {
+        'widget': [],
+    }
+    include_css = {
+        'widget': [],
+    }
+
+    @property
+    def routes(self):
+        from . import routes
+        return [routes.widget_routes, routes.api_routes]
+
+    @property
+    def node_settings(self):
+        return self.get_model('NodeSettings')
