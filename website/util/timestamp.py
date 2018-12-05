57880764ce3d00cf95ee278104480a3f9c56f283
Anthony <44076423+rdm-dev09@users.noreply.github.com>
2018-12-05 17:11:30 +0900

    if this fixes the tests, i'll be very confused
    but i'm not sure what else to try

--- website/util/timestamp.py
+++ website/util/timestamp.py
@@ -83,11 +83,11 @@ def get_error_list(pid):
                 'operator_user': operator_user.fullname,
                 'operator_user_id': operator_user._id,
                 'operator_date': operator_date,
-                'verify_result_title': verify_result_title,
-                'creator_name': base_file_data.versions.filter().latest('id').creator.fullname,
-                'creator_email': base_file_data.versions.filter().latest('id').creator.username,
-                'creator_id': base_file_data.versions.filter().latest('id').creator.id,
-                'creator_institution': base_file_data.versions.filter().latest('id').creator.affiliated_institutions.first()
+                'verify_result_title': verify_result_title
+                # 'creator_name': base_file_data.versions.filter().latest('id').creator.fullname,
+                # 'creator_email': base_file_data.versions.filter().latest('id').creator.username,
+                # 'creator_id': base_file_data.versions.filter().latest('id').creator.id,
+                # 'creator_institution': base_file_data.versions.filter().latest('id').creator.affiliated_institutions.first()
             }
         else:
             file_name = os.path.basename(data.path)
