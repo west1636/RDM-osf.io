70a3cede954952b3289363dc0d5db9a361d909a4
rdm-dev09 <example.email@test.com>
2019-02-19 15:29:31 +0900

    add spot for UPKI implementation

--- website/util/timestamp.py
+++ website/util/timestamp.py
@@ -441,6 +441,9 @@ def userkey_generation(guid):
             logger.exception(error)
             raise
 
+    else:
+        pass
+
 def create_rdmuserkey_info(user_id, key_name, key_kind, date):
     userkey_info = RdmUserKey()
     userkey_info.guid = user_id
@@ -517,6 +520,9 @@ class AddTimestamp:
 
             return TimeStampTokenVerifyCheck().timestamp_check(
                 guid, file_info, project_id, file_name, tmp_dir)
+
+        else:
+            pass
 
 
 class TimeStampTokenVerifyCheck:
