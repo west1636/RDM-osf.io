7dbdd322e0e3326d68ad3330178b1d9abb46b90a
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-10-15 09:51:49 +0900

    flake8 fix

--- framework/auth/views.py
+++ framework/auth/views.py
@@ -419,7 +419,6 @@ def auth_logout(auth, redirect_url=None, next_url=None):
     #   the redirect url is set to CAS service url.
     #   support `reauth`
     #logger.error('----{}::{}({})from:{}::{}({})'.format(inspect.getframeinfo(inspect.currentframe())[0], inspect.getframeinfo(inspect.currentframe())[2], inspect.getframeinfo(inspect.currentframe())[1], inspect.stack()[1][1], inspect.stack()[1][3], inspect.stack()[1][2]))
-    #raise Exception('logout testing.............................!') 
     logger.error('----{}::{}({})from:{}::{}({})'.format(inspect.getframeinfo(inspect.currentframe())[0], inspect.getframeinfo(inspect.currentframe())[2], inspect.getframeinfo(inspect.currentframe())[1], inspect.stack()[1][1], inspect.stack()[1][3], inspect.stack()[1][2]))
     update_default_storage(auth.user)
     # logout/?next=<an OSF verified next url>
