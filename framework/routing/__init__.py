8fdca27bab121840f08dae5066fc312c41a93ddd
west1636 <nishikawa@nii.ac.jp>
2020-06-02 10:15:28 +0900

    #19698 revise mako render arg for encoding

--- framework/routing/__init__.py
+++ framework/routing/__init__.py
@@ -235,7 +235,7 @@ def render_mako_string(tpldir, tplname, data, trust=True):
     tpl = mako_cache.get(tplname)
     if tpl is None:
         with open(os.path.join(tpldir, tplname)) as f:
-            tpl_text = f.read().decode(encoding='utf-8').encode(encoding='utf-8')
+            tpl_text = f.read()
         tpl = Template(
             tpl_text,
             format_exceptions=show_errors,
@@ -253,7 +253,7 @@ def render_mako_string(tpldir, tplname, data, trust=True):
     request.babel_translations = catalog
     app.babel_translations = catalog
 
-    return tpl.render(**data)
+    return tpl.render(input_encoding='utf-8', output_encoding='utf-8')
 
 
 renderer_extension_map = {
