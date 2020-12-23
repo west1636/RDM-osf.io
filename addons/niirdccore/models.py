dc8a21001251b89bef726c19d5e43154f973ad0f
ivis-sasaki <junko.sasaki@ivis.co.jp>
2020-12-23 09:17:11 +0900

    NII RDC Coreアドオン：アドオン追加機能の実装  22474: (4). JupyterHub利用環境のプロビジョニング機能の実装  22477: (4). RDC Core アドオンのデフォルトアドオン化の実装

--- addons/niirdccore/models.py
+++ addons/niirdccore/models.py
@@ -3,9 +3,15 @@ import logging
 import json
 
 from django.db import models
+from django.db.models import Subquery
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from osf.models import Contributor, RdmAddonOption, AbstractNode
 from osf.models.node import Node
 
 from . import settings
+from . import SHORT_NAME
 from addons.base.models import BaseNodeSettings
 from website import settings as ws_settings
 
@@ -18,3 +24,28 @@ class NodeSettings(BaseNodeSettings):
 
     def get_dmr_api_key(self):
         return settings.DMR_API_KEY
+
+    @receiver(post_save, sender=Node)
+    def add_niirdccore_addon(sender, instance, created, **kwargs):
+        if SHORT_NAME not in ws_settings.ADDONS_AVAILABLE_DICT:
+            return
+
+        if instance.has_addon(SHORT_NAME):
+            # add済みの場合は終了
+            return
+
+        inst_ids = instance.affiliated_institutions.values('id')
+        addon_option = RdmAddonOption.objects.filter(
+            provider=SHORT_NAME,
+            institution_id__in=Subquery(inst_ids),
+            management_node__isnull=False,
+            is_allowed=True
+        ).first()
+        if addon_option is None:
+            return
+        if addon_option.organizational_node is not None and \
+                not addon_option.organizational_node.is_contributor(instance.creator):
+            return
+
+        instance.add_addon(SHORT_NAME, auth=None, log=False)
+
--- addons/niirdccore/routes.py
+++ addons/niirdccore/routes.py
@@ -6,17 +6,8 @@ from framework.routing import Rule, json_renderer
 from . import SHORT_NAME
 from . import views
 
-settings_routes = {
-    'rules': [],
-    'prefix': '/api/v1',
-}
-
 api_routes = {
     'rules': [
-        Rule([
-            '/project/<pid>/{}/settings'.format(SHORT_NAME),
-            '/project/<pid>/node/<nid>/{}/settings'.format(SHORT_NAME),
-        ], 'get', views.niirdccore_get_config, json_renderer),
         Rule([
             '/project/<pid>/{}/settings'.format(SHORT_NAME),
             '/project/<pid>/node/<nid>/{}/settings'.format(SHORT_NAME),
