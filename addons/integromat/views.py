3fc7f0587550162a162e4dc91f2e8df2fbad4404
west1636 <nishikawa@nii.ac.jp>
2021-01-05 15:17:34 +0900

    (try1-2) api for itnegromat

--- addons/integromat/views.py
+++ addons/integromat/views.py
@@ -172,7 +172,7 @@ def integromat_api_call(**kwargs):
 def integromat_create_meeting_info(**kwargs):
 
     logger.info('integromat_create_meeting_info start')
-    logger.info('kwargs:' + dict(kwargs))
+    logger.info('kwargs:' + str(dict(kwargs)))
 
     if kwargs['meetingAppName'] == settings.MICROSOFT_TEAMS:
         logger.info('meetingAppName')
