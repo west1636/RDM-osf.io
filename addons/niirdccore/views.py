cdeb3e0a50df69cd4cf35ef03faadb7d94b7ce6d
ivis-kuwata <jumpei.kuwata@ivis.co.jp>
2021-01-08 08:23:49 +0900

    (6) 購読申請API、(7) DMP送信APIを実装

--- addons/niirdccore/views.py
+++ addons/niirdccore/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from rest_framework import status as http_status
 from flask import request
+from django.db.models import Subquery
 import logging
 import requests
 
+from osf.models.node import Node
 from . import SHORT_NAME
 from . import settings
 from framework.exceptions import HTTPError
@@ -14,6 +16,9 @@ from website.project.decorators import (
 )
 from website.ember_osf_web.views import use_ember_app
 from addons.jupyterhub.apps import JupyterhubAddonAppConfig
+from addons.niirdccore.models import AddonList
+
+from addons import *
 from addons.jupyterhub.models import NodeSettings
 
 logger = logging.getLogger(__name__)
@@ -77,3 +82,42 @@ def niirdccore_get_dmp_info(**kwargs):
 @must_have_addon(SHORT_NAME, 'node')
 def project_niirdccore(**kwargs):
     return use_ember_app()
+
+@must_have_permission('admin')
+@must_have_addon(SHORT_NAME, 'node')
+def niirdccore_apply_dmp_subscribe(**kwargs):
+    node = kwargs['node']
+    addon = node.get_addon(SHORT_NAME)
+    addon_list = AddonList()
+
+    addon_list.set_node_id(node._id)
+    addon_list.set_dmp_id(addon.get_dmp_id())
+    addon_list.set_addon_id(kwargs['addon_id'])
+    addon_list.set_callback(kwargs['callback'])
+    addon_list.set_owner(node.get_addon(SHORT_NAME))
+
+    return
+
+def niirdccore_dmp_notification(**kwargs):
+
+    # コールバック関数を呼び出す関数
+    def _notification_handler(func, **kwargs):
+        return func(**kwargs)
+
+    # リクエストボディ取得
+    try:
+        dmp_record = request.json['dmp']
+        dmp_id = request.json['dmp']['redboxOid']
+    except KeyError:
+        raise HTTPError(http_status.HTTP_400_BAD_REQUEST)
+
+    addon_list = AddonList.objects.filter(dmp_id=dmp_id)
+
+    for addon in addon_list:
+        # デコレータ対策のため、nodeも引数に含める
+        _notification_handler(
+            func=eval(addon.callback),
+            node=Node.objects.get(guids___id=addon.node_id),
+            dmp_record=dmp_record)
+
+    return
