39e644bd1c4cf348854103dbd31618d32c15410f
west1636 <nishikawa@nii.ac.jp>
2021-02-20 01:09:45 +0900

    revise views.py

--- addons/integromat/settings/defaults.py
+++ addons/integromat/settings/defaults.py
@@ -1,8 +1,8 @@
 H_SDK_VERSION='2.0.0'
 INTEGROMAT_API_WHOAMI='https://api.integromat.com/v1/whoami'
 
-ACTION_CREATE_MICROSOFRT_TEAMS_MEETING='createMicrosoftTeamsMeeting'
-ACTION_UPDATE_MICROSOFRT_TEAMS_MEETING='updateMicrosoftTeamsMeeting'
-ACTION_DELETE_MICROSOFRT_TEAMS_MEETING='deleteMicrosoftTeamsMeeting'
+ACTION_CREATE_MICROSOFT_TEAMS_MEETING='createMicrosoftTeamsMeeting'
+ACTION_UPDATE_MICROSOFT_TEAMS_MEETING='updateMicrosoftTeamsMeeting'
+ACTION_DELETE_MICROSOFT_TEAMS_MEETING='deleteMicrosoftTeamsMeeting'
 
 MICROSOFT_TEAMS='MicrosoftTeams'
