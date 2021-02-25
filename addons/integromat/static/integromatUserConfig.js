5db9c19a9537f05097dfe2f3199519ef232eaeab
west1636 <nishikawa@nii.ac.jp>
2021-02-25 15:22:29 +0900

    Revert "integromat addon set followed the tutorial(1)"
    This reverts commit 9474fc328a223263113d1887d52b42237f188276.

--- addons/integromat/static/integromatUserConfig.js
+++ addons/integromat/static/integromatUserConfig.js
@@ -12,83 +12,64 @@ require('js/osfToggleHeight');
 var language = require('js/osfLanguage').Addons.integromat;
 var osfHelpers = require('js/osfHelpers');
 var addonSettings = require('js/addonSettings');
-var ChangeMessageMixin = require('js/changeMessage');
+var oop = require('js/oop');
+var OAuthAddonSettingsViewModel = require('js/addonSettings.js').OAuthAddonSettingsViewModel;
+
 var ExternalAccount = addonSettings.ExternalAccount;
 
 var $modal = $('#integromatCredentialsModal');
 
-function ViewModel(url) {
-    var self = this;
 
-    self.properName = 'Integromat';
-    self.accessKey = ko.observable();
-    self.secretKey = ko.observable();
-    self.account_url = '/api/v1/settings/integromat/accounts/';
-    self.accounts = ko.observableArray();
+var ViewModel = oop.extend(OAuthAddonSettingsViewModel,{
+    constructor: function(url){
+        var self = this;
+        self.name = 'integromat';
+        self.properName = 'Integromat';
+        self.accounts = ko.observableArray();
+        self.message = ko.observable('');
+        self.messageClass = ko.observable('');
+        const otherString = 'Other (Please Specify)';
 
-    self.integromatApiToken = ko.observable();
-    self.integromatWebhookUrl = ko.observable();
+        self.url = url;
+        self.properName = 'Integromat';
+        self.integromatApiToken = ko.observable();
+        self.integromatWebhookUrl = ko.observable();
+        self.urls = ko.observable({});
+        self.hosts = ko.observableArray([]);
+        self.selectedHost = ko.observable();    // Host specified in select element
+        self.customHost = ko.observable();      // Host specified in input element
+        // Whether the initial data has been loaded
+        self.loaded = ko.observable(false);
 
-    ChangeMessageMixin.call(self);
+        self.userGuid = ko.observable();
+        self.microsoftTeamsUserObject = ko.observable();
+        self.microsoftTeamsMail = ko.observable();
 
-    /** Reset all fields from Integromat credentials input modal */
-    self.clearModal = function() {
+        self.showApiTokenInput = ko.pureComputed(function() {
+            return Boolean(self.selectedHost());
+        });
+    },
+    clearModal: function() {
+        /** Reset all fields from Integromat host selection modal */
+        var self = this;
         self.message('');
         self.messageClass('text-info');
         self.integromatApiToken(null);
         self.integromatWebhookUrl(null);
-    };
-    /** Send POST request to authorize Integromat */
-    self.connectAccount = function() {
-        // Selection should not be empty
-        if (!self.integromatApiToken() ){
-            self.changeMessage('Please enter an API token.', 'text-danger');
-            return;
-        }
-        if (!self.integromatWebhookUrl() ){
-            self.changeMessage('Please enter an Webhook URL.', 'text-danger');
-            return;
-        }
 
-        return osfHelpers.postJSON(
-            self.account_url,
-            ko.toJS({
-                integromat_api_token: self.integromatApiToken(),
-                integromat_webhook_url: self.integromatWebhookUrl()
-            })
-        ).done(function() {
-            self.clearModal();
-            $modal.modal('hide');
-            self.updateAccounts();
-
-        }).fail(function(xhr, textStatus, error) {
-            var errorMessage = (xhr.status === 400 && xhr.responseJSON.message !== undefined) ? xhr.responseJSON.message : language.authError;
-            self.changeMessage(errorMessage, 'text-danger');
-            Raven.captureMessage('Could not authenticate with Integromat', {
-                extra: {
-                    url: self.account_url,
-                    textStatus: textStatus,
-                    error: error
-                }
-            });
-        });
-    };
-
-    self.updateAccounts = function() {
-        return $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: 'json'
-        }).done(function (data) {
+    },
+    updateAccounts: function() {
+        var self = this;
+        var url = self.urls().accounts;
+        var request = $.get(url);
+        request.done(function(data) {
             self.accounts($.map(data.accounts, function(account) {
                 var externalAccount =  new ExternalAccount(account);
-                externalAccount.integromatApiToken = account.integromat_api_token;
-                externalAccount.integromatWebhookUrl = account.integromat_webhook_url;
                 return externalAccount;
             }));
             $('#integromat-header').osfToggleHeight({height: 160});
-        }).fail(function(xhr, status, error) {
-            self.changeMessage(language.userSettingsError, 'text-danger');
+        });
+        request.fail(function(xhr, status, error) {
             Raven.captureMessage('Error while updating addon account', {
                 extra: {
                     url: url,
@@ -97,60 +78,83 @@ function ViewModel(url) {
                 }
             });
         });
-    };
-
-    self.askDisconnect = function(account) {
+        return request;
+    },
+    connectAccount : function() {
         var self = this;
-        bootbox.confirm({
-            title: 'Disconnect Integromat Account?',
-            message: '<p class="overflow">' +
-                'Are you sure you want to disconnect the Integromat account <strong>' +
-                osfHelpers.htmlEscape(account.name) + '</strong>? This will revoke access to Integromat for all projects associated with this account.' +
-                '</p>',
-            callback: function (confirm) {
-                if (confirm) {
-                    self.disconnectAccount(account);
-                }
-            },
-            buttons:{
-                confirm:{
-                    label:'Disconnect',
-                    className:'btn-danger'
-                }
-            }
-        });
-    };
+        // Selection should not be empty
+        if (!self.integromatApiToken() ){
+            self.setMessage('Please enter an API token.', 'text-danger');
+            return;
+        }
+        if (!self.integromatWebhookUrl() ){
+            self.setMessage('Please enter an Webhook URL.', 'text-danger');
+            return;
+        }
 
-    self.disconnectAccount = function(account) {
-        var self = this;
-        var url = '/api/v1/oauth/accounts/' + account.id + '/';
-        var request = $.ajax({
-            url: url,
-            type: 'DELETE'
-        });
-        request.done(function(data) {
+        var url = self.urls().create;
+        return osfHelpers.postJSON(
+            url,
+            ko.toJS({
+                integromat_api_token: self.integromatApiToken(),
+                integromat_webhook_url: self.integromatWebhookUrl()
+            })
+        ).done(function() {
+            self.clearModal();
+            $modal.modal('hide');
+            $('#integromatCredentialsModal').modal('hide');
             self.updateAccounts();
+            self.authSuccessCallback();
+
+        }).fail(function(xhr, textStatus, error) {
+            var errorMessage = (xhr.status === 401) ? '401' : 'Invalid integromat server';
+            self.changeMessage(errorMessage, 'text-danger');
+            Raven.captureMessage('Could not authenticate with Integromat', {
+                url: self.url,
+                textStatus: textStatus,
+                error: error
+            });
         });
-        request.fail(function(xhr, status, error) {
-            Raven.captureMessage('Error while removing addon authorization for ' + account.id, {
+    },
+    authSuccessCallback: function() {
+        // Override for NS-specific auth success behavior
+        // TODO: generalize this when rewriting addon configs for ember
+        return;
+    },
+    fetch: function() {
+        // Update observables with data from the server
+        var self = this;
+        $.ajax({
+            url: self.url,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (response) {
+            var data = response.result;
+            self.urls(data.urls);
+            self.loaded(true);
+            self.updateAccounts();
+        }).fail(function (xhr, textStatus, error) {
+            self.setMessage('Could not retrieve settings', 'text-danger');
+            Raven.captureMessage('Could not GET Integromat settings', {
                 extra: {
-                    url: url,
-                    status: status,
+                    url: self.url,
+                    textStatus: textStatus,
                     error: error
                 }
             });
         });
-        return request;
-    };
-
-    self.selectionChanged = function() {
-        self.changeMessage('','');
-    };
-
-    self.updateAccounts();
-}
-
-$.extend(ViewModel.prototype, ChangeMessageMixin.prototype);
+    },
+    selectionChanged: function() {
+        var self = this;
+        self.setMessage('','');
+    },
+    addMicrosoftTeamsUser : function() {
+        var self = this;
+    },
+    deleteMicrosoftTeamsUser : function() {
+        var self = this;
+    },
+});
 
 function IntegromatUserConfig(selector, url) {
     // Initialization code
@@ -163,6 +167,6 @@ function IntegromatUserConfig(selector, url) {
 }
 
 module.exports = {
-    IntegromatViewModel: ViewModel,
-    IntegromatUserConfig: IntegromatUserConfig
+    IntegromatUserViewModel: ViewModel,
+    IntegromatUserConfig: IntegromatUserConfig    // for backwards-compat
 };
