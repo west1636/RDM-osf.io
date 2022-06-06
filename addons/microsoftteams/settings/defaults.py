73be5cf8869862ce0173b1e19b9c8199d466f323
west1636 <nishikawa@nii.ac.jp>
2022-06-06 17:31:00 +0900

    fix settings URL

--- addons/microsoftteams/settings/defaults.py
+++ addons/microsoftteams/settings/defaults.py
@@ -1,6 +1,6 @@
 MICROSOFT_GRAPH_API_BASE_URL = 'https://graph.microsoft.com/v1.0/'
 MICROSOFT_GRAPH_API_URL_ORGANIZATION = 'https://graph.microsoft.com/v1.0/organization'
 MICROSOFT_GRAPH_DEFAULT_SCOPE = 'https://graph.microsoft.com/.default'
-MICROSOFT_OELINE_BASE_URL = 'https://login.microsoftonline.com/'
+MICROSOFT_ONLINE_BASE_URL = 'https://login.microsoftonline.com/'
 
 MICROSOFT_TEAMS = 'Microsoft Teams'
