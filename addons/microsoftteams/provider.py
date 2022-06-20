e8a20d12dd3459705d329ed6a7912834102478c1
west1636 <nishikawa@nii.ac.jp>
2022-06-20 14:49:01 +0900

    revise provider.py

--- addons/microsoftteams/provider.py
+++ addons/microsoftteams/provider.py
@@ -9,8 +9,8 @@ class MicrosoftTeamsProvider(object):
     serializer = MicrosoftTeamsSerializer
     client_id = settings.MICROSOFT_365_KEY
     client_secret = settings.MICROSOFT_365_SECRET
-    auth_url_base = '{}{}{}'.format(settings.MICROSOFT_ONLINE_BASE_URL, MICROSOFT_TENANT, '/auth2/v2.0/authorize')
-    callback_url = '{}{}{}'.format(settings.MICROSOFT_ONLINE_BASE_URL, MICROSOFT_TENANT, '/oauth2/v2.0/token')
+    auth_url_base = '{}{}{}'.format(settings.MICROSOFT_ONLINE_BASE_URL, settings.MICROSOFT_TENANT, '/auth2/v2.0/authorize')
+    callback_url = '{}{}{}'.format(settings.MICROSOFT_ONLINE_BASE_URL, settings.MICROSOFT_TENANT, '/oauth2/v2.0/token')
     auto_refresh_url = callback_url
 
     def handle_callback(self, response):
