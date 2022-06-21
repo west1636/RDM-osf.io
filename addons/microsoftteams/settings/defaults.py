f98bd524dedf5bd2f87f4457244ec7f716bf6bc4
west1636 <nishikawa@nii.ac.jp>
2022-06-21 11:16:56 +0900

    revise defaults.py

--- addons/microsoftteams/settings/defaults.py
+++ addons/microsoftteams/settings/defaults.py
@@ -5,7 +5,7 @@ MICROSOFT_GRAPH_API_BASE_URL = 'https://graph.microsoft.com/v1.0/'
 MICROSOFT_GRAPH_API_URL_ORGANIZATION = 'https://graph.microsoft.com/v1.0/organization'
 MICROSOFT_GRAPH_DEFAULT_SCOPE = 'https://graph.microsoft.com/.default'
 MICROSOFT_ONLINE_BASE_URL = 'https://login.microsoftonline.com/'
-MICROSOFT_TENANT = 'common'
+MICROSOFT_TENANT = 'organizations'
 MICROSOFT_API_SCOPE = 'user.read.all calendars.readwrite'
 
 MICROSOFT_TEAMS = 'Microsoft Teams'
