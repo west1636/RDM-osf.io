9da6ea3afd507547891f67de191966c60e53d38c
west1636 <nishikawa@nii.ac.jp>
2022-06-22 08:53:21 +0900

    add fetch_acces_token models.py

--- addons/microsoftteams/models.py
+++ addons/microsoftteams/models.py
@@ -80,6 +80,9 @@ class NodeSettings(BaseOAuthNodeSettings, BaseStorageAddon):
     def after_delete(self, user):
         self.deauthorize(Auth(user=user), log=True)
 
+    def fetch_access_token(self):
+        return self.api.fetch_access_token()
+
 class Attendees(ObjectIDMixin, BaseModel):
 
     user_guid = models.CharField(max_length=255, default=None)
