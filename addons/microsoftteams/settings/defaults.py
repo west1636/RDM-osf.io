a5212a07221eaa5a4802ef24ad9d2dc74b8f737d
west1636 <nishikawa@nii.ac.jp>
2022-07-07 13:39:30 +0900

    change tenant to common

--- addons/microsoftteams/settings/defaults.py
+++ addons/microsoftteams/settings/defaults.py
@@ -5,7 +5,7 @@ MICROSOFT_GRAPH_API_BASE_URL = 'https://graph.microsoft.com/'
 MICROSOFT_GRAPH_API_URL_ORGANIZATION = 'https://graph.microsoft.com/v1.0/organization'
 MICROSOFT_GRAPH_DEFAULT_SCOPE = 'https://graph.microsoft.com/.default'
 MICROSOFT_ONLINE_BASE_URL = 'https://login.microsoftonline.com/'
-MICROSOFT_TENANT = 'organizations'
+MICROSOFT_TENANT = 'common'
 MICROSOFT_API_SCOPE = 'offline_access user.read.all calendars.readwrite'
 
 MICROSOFT_TEAMS_SIGNATURE = '{1}<br><a href="{2}">会議に参加するにはここをクリック</a>'
