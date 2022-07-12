a0277f8bb6454afef04b915c7b212e1a0a65ed45
west1636 <nishikawa@nii.ac.jp>
2022-07-12 15:24:25 +0900

    change scope teams

--- addons/microsoftteams/settings/defaults.py
+++ addons/microsoftteams/settings/defaults.py
@@ -6,7 +6,7 @@ MICROSOFT_GRAPH_API_URL_ORGANIZATION = 'https://graph.microsoft.com/v1.0/organiz
 MICROSOFT_GRAPH_DEFAULT_SCOPE = 'https://graph.microsoft.com/.default'
 MICROSOFT_ONLINE_BASE_URL = 'https://login.microsoftonline.com/'
 MICROSOFT_TENANT = 'common'
-MICROSOFT_API_SCOPE = 'offline_access user.read.all calendars.readwrite'
+MICROSOFT_API_SCOPE = 'openid profile offline_access user.read.all calendars.readwrite'
 
 MICROSOFT_TEAMS_SIGNATURE = '{1}<br><a href="{2}">会議に参加するにはここをクリック</a>'
 
