11cae4b6044ea355f611bbd6c2123723f8a5f2b8
west1636 <nishikawa@nii.ac.jp>
2022-07-27 15:05:28 +0900

    add column external account to each app

--- addons/zoommeetings/models.py
+++ addons/zoommeetings/models.py
@@ -102,5 +102,6 @@ class ZoomMeetings(ObjectIDMixin, BaseModel):
     content = models.TextField(blank=True, null=True, max_length=10000)
     join_url = models.TextField(max_length=512)
     meetingid = models.TextField(max_length=512)
+    external_account = models.ForeignKey(ExternalAccount, null=True, blank=True, default=None)
     node_settings = models.ForeignKey(NodeSettings, null=False, blank=False, default=None)
 
