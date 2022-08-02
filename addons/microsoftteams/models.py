3a0b1cbd8aa64b3778eb30a9631b9be5788ca54c
west1636 <nishikawa@nii.ac.jp>
2022-08-03 07:50:59 +0900

    Revert "add column app_name"
    This reverts commit 070f976a83681b15e472df41d8227f959727abdc.

--- addons/microsoftteams/models.py
+++ addons/microsoftteams/models.py
@@ -116,6 +116,5 @@ class MicrosoftTeams(ObjectIDMixin, BaseModel):
     content = models.TextField(blank=True, null=True, max_length=10000)
     join_url = models.TextField(max_length=512)
     meetingid = models.TextField(max_length=512)
-    app_name = models.CharField(max_length=128, default=settings.MICROSOFT_TEAMS)
     external_account = models.ForeignKey(ExternalAccount, null=True, blank=True, default=None)
     node_settings = models.ForeignKey(NodeSettings, null=False, blank=False, default=None)
