bee7f3f19a76046c17f146e899aba8867d87e9c0
west1636 <nishikawa@nii.ac.jp>
2022-08-08 21:37:04 +0900

    revise defaults.py

--- addons/webexmeetings/settings/defaults.py
+++ addons/webexmeetings/settings/defaults.py
@@ -4,3 +4,6 @@ WEBEX_API_BASE_URL = 'https://webexapis.com/'
 WEBEX_API_SCOPE = 'spark:kms meeting:schedules_read spark:people_read meeting:schedules_write'
 
 WEBEX_MEETINGS = 'Webex Meetings'
+
+EXPIRY_TIME = 60 * 60 * 24 * 90  # 90 days
+REFRESH_TIME = 5 * 60  # 5 minutes
