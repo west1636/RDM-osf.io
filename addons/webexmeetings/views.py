ffde51d22aa3bbba7552a5dc1eaed3f1434b66bb
west1636 <nishikawa@nii.ac.jp>
2022-08-23 12:55:51 +0900

    revise views.py

--- addons/webexmeetings/views.py
+++ addons/webexmeetings/views.py
@@ -211,7 +211,7 @@ def webexmeetings_register_email(**kwargs):
     guid = requestDataJson['guid']
     fullname = ''
     email = requestDataJson['email']
-    is_guest = requestDataJson['is_guest']
+    is_guest = requestDataJson.get('is_guest', true)
     actionType = requestDataJson['actionType']
 
     nodeSettings = models.NodeSettings.objects.get(_id=addon._id)
