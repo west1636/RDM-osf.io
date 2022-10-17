4ec07eda6a79ba3f65d542273ec3b047fc4a05b9
west1636 <nishikawa@nii.ac.jp>
2022-10-17 16:23:20 +0900

    ZOOM deauth notification(4)

--- addons/base/views.py
+++ addons/base/views.py
@@ -1346,6 +1346,7 @@ def zoommeetings_deauthorize_notification(**kwargs):
 
     result = False
     authorizationHeader = request.headers.get('authorization')
+    logger.info('request header authorization:' + str(authorizationHeader))
     if authorizationHeader == zoom_meetings_settings.ZOOM_MEETINGS_SECRET_TOKEN:
         result = True
         logger.info('Recieved Zoom Meetings Webhook')
