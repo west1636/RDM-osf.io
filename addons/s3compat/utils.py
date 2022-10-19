862f7f91ed2c8491adf7f526a2429ea48ffd5591
west1636 <nishikawa@nii.ac.jp>
2022-10-19 09:30:15 +0900

    validate_certs is false when port is 10443 or 11443(2)

--- addons/s3compat/utils.py
+++ addons/s3compat/utils.py
@@ -50,13 +50,14 @@ def connect_s3compat(host=None, access_key=None, secret_key=None, node_settings=
     logger.info('connect_s3compat host:' + str(host))
     logger.info('connect_s3compat port:' + str(port))
     is_secure = True if port == 443 or port == 10443 or port == 11443 else False 
-    validate_certs = False port == 10443 or port == 11443 else True
+    validate_certs = False if port == 10443 or port == 11443 else True
     logger.info('is_secure:' + str(is_secure))
+    logger.info('validate_certs:' + str(validate_certs))
     return S3CompatConnection(access_key, secret_key,
                               calling_format=OrdinaryCallingFormat(),
                               host=host,
                               port=port,
-                              is_secure=is_secure
+                              is_secure=is_secure,
                               validate_certs=validate_certs)
 
 
